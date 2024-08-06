6f3dad5efc9eb34ad5073feb81e92a982794db48
sherelynn <sherelynnb@missionreadyhq.com>
2024-08-06 16:07:22 +1200

    Initialise initialState for add-ons section in avSlice.js

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,22 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+      img: "/images/projector.jpg",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
+    },
+    {
+      img: "/images/speaker.jpg",
+      name: "Speakers",
+      cost: 35,
+      quantity: 0,
+    },
+    {
+      img: "/images/microphone.jpg",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0,
+    },
+    {
+      img: "/images/whiteboard.jpg",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
+    {
+      img: "/images/signage.jpg",
+      name: "Signage",
+      cost: 80,
+      quantity: 0,
+    },
   ],
 
-
   reducers: {
-    incrementAvQuantity: (state, action) => {
-      
-    },
-    decrementAvQuantity: (state, action) => {
-     
-    },
+    incrementAvQuantity: (state, action) => {},
+    decrementAvQuantity: (state, action) => {},
   },
-});
+})
 
-export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
+export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions
 
-export default avSlice.reducer;
+export default avSlice.reducer
